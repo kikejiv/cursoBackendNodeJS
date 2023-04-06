934c663b16f66bea3e8e8021d068da90457843c6
Jose Valencia <tecnicojiv@gmail.com>
2023-04-06 18:42:45 -0500

    clase 02 instalacion de expres y primer servidor http

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log ('My App')
+const express = require('express'); //traemos el modulo de express
+const app = express(); // asi crearmos nuestra aplicacion
+const port = 3000; // definimo el puerto donde queremos que nos corra
+
+app.get('/', (req, res) => { // para definir la ruta / seguido del callback
+  res.send('Hola mi server en express'); //retorna
+})
+
+app.listen(port, () => {
+  console.log('Mi port ' + port);
+});
+
+
