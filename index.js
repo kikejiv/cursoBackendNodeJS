daee686fb6785dba639837fbbf09907010c66650
Jose Valencia <tecnicojiv@gmail.com>
2023-04-06 22:08:18 -0500

    clase 03 routing con express

--- index.js
+++ index.js
@@ -4,9 +4,20 @@ const port = 3000; // definimo el puerto donde queremos que nos corra
 
 app.get('/', (req, res) => { // para definir la ruta / seguido del callback
   res.send('Hola mi server en express'); //retorna
-})
+});
 
-app.listen(port, () => {
+app.get('/nueva-ruta', (req, res) => { // para definir la ruta / seguido del callback
+  res.send('Hola soy una nueva ruta'); //retorna
+});
+
+app.get('/productos', (req, res) => { // para definir la ruta / seguido del callback
+  res.json({
+    name: 'Producto 1',
+    price: 1000
+  }); //retorna
+});
+
+app.listen(port, () => { //es para que escuche el puertoo
   console.log('Mi port ' + port);
 });
 
